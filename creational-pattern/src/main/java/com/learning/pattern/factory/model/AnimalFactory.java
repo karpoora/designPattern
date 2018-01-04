c4151c1f9c463a5e790ae9116bf3ca6d2052f243
Sundarapandian, Karpoora <karpoora.sundarapandian@astrazeneca.com>
2018-01-04 17:12:38 +0530

    implement Null Object Pattern.

--- creational-pattern/src/main/java/com/learning/pattern/factory/model/AnimalFactory.java
+++ creational-pattern/src/main/java/com/learning/pattern/factory/model/AnimalFactory.java
@@ -10,14 +10,17 @@ public class AnimalFactory {
     HashMap<String,String> animalMap=new HashMap();
 
     public AnimalFactory(){
-        animalMap.put("bat","com.learning.pattern.model.factory.Bat");
-        animalMap.put("cat","com.learning.pattern.model.factory.Cat");
-        animalMap.put("dog","com.learning.pattern.model.factory.Dog");
-        animalMap.put("elephant","com.learning.pattern.model.factory.Elephant");
+        animalMap.put("bat","com.learning.pattern.factory.model.Bat");
+        animalMap.put("cat","com.learning.pattern.factory.model.Cat");
+        animalMap.put("dog","com.learning.pattern.factory.model.Dog");
+        animalMap.put("elephant","com.learning.pattern.factory.model.Elephant");
     }
 
     public Animal getAnimal(String animalName) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Animal animal=(Animal) Class.forName(animalMap.get(animalName)).newInstance();
+        String NoAnimalClass = "com.learning.pattern.factory.model.NoAnimal";
+        String animalClassFromMap = animalMap.get(animalName);
+        String animalClassName = animalClassFromMap !=null? animalClassFromMap : NoAnimalClass;
+        Animal animal=(Animal) Class.forName(animalClassName).newInstance();
         return animal;
     }
 }
---
+++ creational-pattern/src/main/java/com/learning/pattern/factory/model/NoAnimal.java
@@ -0,0 +1,10 @@
+package com.learning.pattern.factory.model;
+
+/**
+ * Created by krvh271 on 1/4/18.
+ */
+public class NoAnimal implements Animal {
+    public String sound() {
+        return "No Sound";
+    }
+}
